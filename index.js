e0adaa1870de8c48c6c364b240e3e7614c0753f0
Timo Uhlmann <t.uhlmann@zeroseven.de>
2013-04-19 10:48:18 +0200

    Implemented all missing methods from NodeJS as dummies

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
+/*
+void TCPWrap::Initialize(Handle<Object> target) {
+  HandleWrap::Initialize(target);
+  StreamWrap::Initialize(target);
+
+  HandleScope scope(node_isolate);
+
+  Local<FunctionTemplate> t = FunctionTemplate::New(New);
+  t->SetClassName(String::NewSymbol("TCP"));
+
+  t->InstanceTemplate()->SetInternalFieldCount(1);
+
+  enum PropertyAttribute attributes =
+      static_cast<PropertyAttribute>(v8::ReadOnly | v8::DontDelete);
+  t->InstanceTemplate()->SetAccessor(String::New("fd"),
+                                     StreamWrap::GetFD,
+                                     NULL,
+                                     Handle<Value>(),
+                                     v8::DEFAULT,
+                                     attributes);
+
+  NODE_SET_PROTOTYPE_METHOD(t, "close", HandleWrap::Close);
+
+  NODE_SET_PROTOTYPE_METHOD(t, "ref", HandleWrap::Ref);
+  NODE_SET_PROTOTYPE_METHOD(t, "unref", HandleWrap::Unref);
+
+  NODE_SET_PROTOTYPE_METHOD(t, "readStart", StreamWrap::ReadStart);
+  NODE_SET_PROTOTYPE_METHOD(t, "readStop", StreamWrap::ReadStop);
+  NODE_SET_PROTOTYPE_METHOD(t, "shutdown", StreamWrap::Shutdown);
+
+  NODE_SET_PROTOTYPE_METHOD(t, "writeBuffer", StreamWrap::WriteBuffer);
+  NODE_SET_PROTOTYPE_METHOD(t, "writeAsciiString", StreamWrap::WriteAsciiString);
+  NODE_SET_PROTOTYPE_METHOD(t, "writeUtf8String", StreamWrap::WriteUtf8String);
+  NODE_SET_PROTOTYPE_METHOD(t, "writeUcs2String", StreamWrap::WriteUcs2String);
+
+  NODE_SET_PROTOTYPE_METHOD(t, "open", Open);
+  NODE_SET_PROTOTYPE_METHOD(t, "bind", Bind);
+  NODE_SET_PROTOTYPE_METHOD(t, "listen", Listen);
+  NODE_SET_PROTOTYPE_METHOD(t, "connect", Connect);
+  NODE_SET_PROTOTYPE_METHOD(t, "bind6", Bind6);
+  NODE_SET_PROTOTYPE_METHOD(t, "connect6", Connect6);
+  NODE_SET_PROTOTYPE_METHOD(t, "getsockname", GetSockName);
+  NODE_SET_PROTOTYPE_METHOD(t, "getpeername", GetPeerName);
+  NODE_SET_PROTOTYPE_METHOD(t, "setNoDelay", SetNoDelay);
+  NODE_SET_PROTOTYPE_METHOD(t, "setKeepAlive", SetKeepAlive);
+
+#ifdef _WIN32
+  NODE_SET_PROTOTYPE_METHOD(t, "setSimultaneousAccepts", SetSimultaneousAccepts);
+#endif
+
+  tcpConstructor = Persistent<Function>::New(node_isolate, t->GetFunction());
+
+  onconnection_sym = NODE_PSYMBOL("onconnection");
+  oncomplete_sym = NODE_PSYMBOL("oncomplete");
+
+  target->Set(String::NewSymbol("TCP"), tcpConstructor);
+} */
+
+
+
 var noop = function() {};
 
 function TCP() {
@@ -5,31 +65,26 @@ function TCP() {
   this.writeQueueSize = 0;
 }
 
-TCP.prototype.connect = function(address, port) {
-  var self = this;
-  var req = {
-    oncomplete: noop
-  };
-  chrome.socket.create('tcp', undefined, function (createInfo) {
-    console.log('create callback', createInfo);
-    self._socketId = createInfo.socketId;
-    chrome.socket.connect(self._socketId, address, port, function(result) {
-      console.log('connect callback', result);
-      req.oncomplete(0, self, result, true, true);
-    });
-  });
-  return req;
+TCP.prototype.close = function() {
+  this.readStop();
+  chrome.socket.destroy(this._socketId);
+};
+
+TCP.prototype.ref = function() {
+  console.log('TCP.ref', arguments);
+};
+
+TCP.prototype.unref = function() {
+  console.log('TCP.unref', arguments);
 };
 
 TCP.prototype.readStart = function() {
   console.log('TCP.readStart');
-  this._readHandle = setInterval(this.read.bind(this), 500);
+  this._readHandle = setInterval(read(this), 500);
 };
 
-TCP.prototype.read = function() {
-  // console.log('TCP.read');
-  var self = this;
-  chrome.socket.read(this._socketId, undefined, function(readInfo) {
+var read = function(self) {
+  chrome.socket.read(self._socketId, undefined, function(readInfo) {
     if(readInfo.resultCode > 0) {
       var data = readInfo.data;
       var view = new Uint8Array(data);
@@ -42,6 +97,11 @@ TCP.prototype.read = function() {
 TCP.prototype.readStop = function() {
   clearInterval(this._readHandle);
 };
+
+TCP.prototype.shutdown = function() {
+  console.log('TCP.shutdown', arguments);
+};
+
 
 TCP.prototype.writeBuffer = function(data) {
   if(data.constructor.name !== 'ArrayBuffer') {
@@ -66,9 +126,62 @@ TCP.prototype.writeBuffer = function(data) {
   return req;
 };
 
-TCP.prototype.close = function() {
-  this.readStop();
-  chrome.socket.destroy(this._socketId);
+TCP.prototype.writeAsciiString = function() {
+  console.log('TCP.writeAsciiString', arguments);
+};
+
+TCP.prototype.writeUtf8String = function() {
+  console.log('TCP.writeUtf8String', arguments);
+};
+
+TCP.prototype.writeUcs2String = function() {
+  console.log('TCP.writeUcs2String', arguments);
+};
+
+TCP.prototype.open = function() {
+  console.log('TCP.open', arguments);
+};
+
+TCP.prototype.bind = function() {
+  console.log('TCP.bind', arguments);
+};
+
+TCP.prototype.listen = function() {
+  console.log('TCP.listen', arguments);
+};
+
+TCP.prototype.connect = function(address, port) {
+  var self = this;
+  var req = {
+    oncomplete: noop
+  };
+  chrome.socket.create('tcp', undefined, function (createInfo) {
+    self._socketId = createInfo.socketId;
+    chrome.socket.connect(self._socketId, address, port, function(result) {
+      req.oncomplete(0, self, result, true, true);
+    });
+  });
+  return req;
+};
+
+TCP.prototype.bind6 = function() {
+  console.log('TCP.bind6', arguments);
+};
+
+TCP.prototype.connect6 = function() {
+  console.log('TCP.connect6', arguments);
+};
+
+TCP.prototype.getsocketname = function() {
+  console.log('TCP.getsocketname', arguments);
+};
+
+TCP.prototype.setNoDelay = function() {
+  console.log('TCP.setNoDelay', arguments);
+};
+
+TCP.prototype.setKeepAlive = function() {
+  console.log('TCP.setKeepAlive', arguments);
 };
 
 exports.TCP = TCP;
